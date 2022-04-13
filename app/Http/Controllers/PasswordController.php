58df6c9fe5a5c723e74bdb93a000e0daf9bd86b8
popo888 <422631148@qq.com>
2022-04-13 17:17:53 +0800

    访问限流

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 
 class PasswordController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('throttle:2,1', [
+            'only' => ['showLinkRequestForm']
+        ]);
+
+        $this->middleware('throttle:3,10', [
+            'only' => ['sendResetLinkEmail']
+        ]);
+    }
+
     public function showLinkRequestForm()
     {
         return view('auth.passwords.email');
