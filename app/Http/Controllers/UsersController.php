532edba6c9eec759f8dc9d4426b7b07a65c76bc3
popo888 <422631148@qq.com>
2022-04-13 17:27:18 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -112,13 +112,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'spring@example.com';
-        $name = 'Spring';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认您的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 }
