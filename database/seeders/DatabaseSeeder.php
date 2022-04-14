09e9b157b77581bc491249c89f704872c99ea023
popo888 <422631148@qq.com>
2022-04-14 11:09:50 +0800

    用户微博列表

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         Model::unguard();
+
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
+
         Model::reguard();
     }
 }
